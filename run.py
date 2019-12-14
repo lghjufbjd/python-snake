7c7fcb567cc4c168f13913339e2355842755edef
rewrew <gienek.eu.pl@gmail.com>
2019-12-14 19:37:52 +0100

    essa

--- run.py
+++ run.py
@@ -1,1 +1,11 @@
+from math import *
+
 print("aaaaaaaaa")
+my_num = 5
+print(str(my_num) + "essa")
+print(min(7, 5))
+print(round(1.5))
+print(sqrt(3.7))
+name = input("name: ")
+print(name)
+friends = ["d", "d", "d"]
